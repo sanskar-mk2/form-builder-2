7447bf67af940597c4e03d767b6ff1a76dc42f8d
Sanskar Tiwari <sanskar.mk2@gmail.com>
2022-09-21 21:48:28 +0530

    📢 Survey publish actions

--- app/Filament/Resources/SurveyResource/Pages/ViewSurvey.php
+++ app/Filament/Resources/SurveyResource/Pages/ViewSurvey.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Resources\SurveyResource\Pages;
 
+use App\Enums\SurveyStatus;
 use App\Filament\Resources\SurveyResource;
+use Filament\Notifications\Notification;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -12,8 +14,44 @@ class ViewSurvey extends ViewRecord
 
     protected function getActions(): array
     {
+        $ret = [];
+        if ($this->record->status == SurveyStatus::Unpublished->value) {
+            $ret[] = Actions\Action::make('publish')
+                ->action('publish')
+                ->requiresConfirmation();
+        } elseif ($this->record->status == SurveyStatus::Published->value) {
+            $ret[] = Actions\Action::make('mark_completed')
+                ->action('mark_completed')
+                ->requiresConfirmation();
+        } else;
+
         return [
             Actions\EditAction::make(),
+            ...$ret,
         ];
+    }
+
+    public function publish()
+    {
+        $this->record->status = SurveyStatus::Published->value;
+        $this->record->save();
+
+        Notification::make()
+            ->title('Published Survey: '.$this->record->name)
+            ->success()
+            ->send();
+        $this->redirect($this->getResource()::getUrl('index'));
+    }
+
+    public function mark_completed()
+    {
+        $this->record->status = SurveyStatus::Completed->value;
+        $this->record->save();
+
+        Notification::make()
+            ->title('Survey Marked Done: '.$this->record->name)
+            ->success()
+            ->send();
+        $this->redirect($this->getResource()::getUrl('index'));
     }
 }
