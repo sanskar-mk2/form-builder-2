8c511f7399ff41a98babf32e8a0b5155f20ea6fb
Sanskar Tiwari <sanskar.mk2@gmail.com>
2022-10-02 20:44:19 +0530

    🔒 Implement canAccessFilament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\Role;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function canAccessFilament(): bool
+    {
+        return $this->role === Role::SuperAdmin->value;
+    }
 
     /**
      * The attributes that are mass assignable.
