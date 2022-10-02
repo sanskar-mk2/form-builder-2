e355492807274693f35ed4a5b1e90b1bbd60d602
Sanskar Tiwari <sanskar.mk2@gmail.com>
2022-10-03 00:28:44 +0530

    👌 Improve drag and drop feature

--- resources/views/components/template-master.blade.php
+++ resources/views/components/template-master.blade.php
@@ -1,13 +1,15 @@
 <template x-for="(content, index) in contents" :key="index">
     <div class="flex flex-col gap-4">
-        <div x-on:dragstart.self="dragging=true;event.dataTransfer.effectAllowed='move';event.dataTransfer.setData('text/plain', index);"
+        <div x-data="{local_drag: false}"
             x-on:dragend="dragging=false;"
-            draggable="true" class="card w-full bg-base-100 shadow-xl">
-            <div class="px-4 py-2 bg-base-300 card-actions items-center justify-between">
+            class="card w-full bg-base-100 shadow-xl">
+            <div x-on:dragstart.self="dragging=true;event.dataTransfer.setData('text/plain', index);"
+                draggable="true"
+                class="px-4 py-2 hover:cursor-move bg-base-300 card-actions items-center justify-between">
                 <h3 class="font-extrabold text-2xl text-secondary" x-text="`${index+1}. ${content.type.toUpperCase()}`"></h3>
                 <x-remove x-on:click="remove(index)" />
             </div>
-            <div class="card-body">
+            <div x-show="!dragging" class="card-body">
                 <input placeholder="name" readonly x-model="content.name" type="text" class="input input-bordered w-full max-w-xs" >
                 <template x-if="content.type=='text' || content.type=='description'">
                     <x-template-text />
@@ -19,13 +21,13 @@
             </div>
         </div>
         <div x-data="{over_me: false}"
-            x-on:drop="console.log('dropped')"
+            x-on:dragend.window="over_me=false;"
             x-on:dragover.prevent="over_me=true"
             x-on:dragleave.prevent="over_me=false"
             x-on:drop.prevent="reorder(parseInt($event.dataTransfer.getData('text/plain')), index)"
-            x-bind:class="over_me ? 'bg-neutral' : 'bg-primary'"
+            x-bind:class="over_me ? 'bg-primary' : 'bg-neutral'"
             x-show="dragging" 
-            class="h-4 rounded-full w-full">
+            class="h-8 rounded-full w-full">
             {{-- None --}}
         </div>
     </div>
